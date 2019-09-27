a538b28a7ee5d37401a3d5d785a55cb29d2efa72
G.A.P.M (Gabriel Pereda) <gabrielmiranda@MacBook-Air-01110011.local>
2019-09-27 13:13:13 -0700

    Commit 1

---
+++ iOSRestaurantes/DetallesRestaurante.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iOSRestaurantes
+//
+//  Created by Alumno on 9/25/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class DetallesRestaurante: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ iOSRestaurantes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iOSRestaurantes
+//
+//  Created by Alumno on 9/25/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Restaurantes"
+    }
+}
